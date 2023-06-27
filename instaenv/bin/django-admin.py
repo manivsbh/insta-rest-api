21f970af6c89a4d008fa9d99936835265c17780f
Manish Cheepa <manishchhipa98@gmail.com>
2023-06-27 09:12:38 +0530

    created app and project

---
+++ instaenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/manishcheepa/Django-rest-framework/insta-rest-api/instaenv/bin/python3.11
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
