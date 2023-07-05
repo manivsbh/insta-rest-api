c5fe2e916b66cd7ea050cae578a884343ecf876a
Manish Cheepa <manishchhipa98@gmail.com>
2023-07-05 17:46:44 +0530

    added authorization token for login

--- insta_api/views.py
+++ insta_api/views.py
@@ -7,6 +7,8 @@ from insta_api import models
 from rest_framework.authentication import TokenAuthentication
 from insta_api import permissions
 from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 
 
@@ -106,3 +108,8 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email')
+
+class UserLoginApiView(ObtainAuthToken):
+    """Handle creating user authetication tokens"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+    
